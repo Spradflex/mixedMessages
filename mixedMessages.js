40486e32a0321ff827bc8971a03c765cb78d3291
Spradflex <79716211+Spradflex@users.noreply.github.com>
2021-06-24 20:49:16 -0500

    JS update; Created HTML CSS

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,17 +1,19 @@
-let quoteArray = [
-    'The best way to get started is to quit talking and begin doing. – Walt Disney',
-    'The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty. – Winston Churchill',
-    'Don’t let yesterday take up too much of today. – Will Rogers',
-    'You learn more from failure than from success. Don’t let it stop you. Failure builds character. – Unknown',
-    'If you are working on something that you really care about, you don’t have to be pushed. The vision pulls you. – Steve Jobs',
-    'Imagine your life is perfect in every respect; what would it look like? – Brian Tracy',
-    'We generate fears while we sit. We overcome them by action. – Dr. Henry Link',
-    'The man who has confidence in himself gains the confidence of others. – Hasidic Proverb',
-    'Fake it until you make it! Act as if you had all the confidence you require until it becomes your reality. – Brian Tracy',
-    'Reading is to the mind, as exercise is to the body. – Brian Tracy'
+
+//Quotes to pull from; 10 for each array
+let quote = [
+    `"The best way to get started is to quit talking and begin doing." – Walt Disney`,
+    `"The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty." – Winston Churchill`,
+    `"Don’t let yesterday take up too much of today." – Will Rogers`,
+    `"You learn more from failure than from success. Don’t let it stop you. Failure builds character." – Unknown`,
+    `"If you are working on something that you really care about, you don’t have to be pushed. The vision pulls you." – Steve Jobs`,
+    `"Imagine your life is perfect in every respect; what would it look like?" – Brian Tracy`,
+    `"We generate fears while we sit. We overcome them by action." – Dr. Henry Link`,
+    `"The man who has confidence in himself gains the confidence of others." – Hasidic Proverb`,
+    `"Fake it until you make it! Act as if you had all the confidence you require until it becomes your reality." – Brian Tracy`,
+    `"Reading is to the mind, as exercise is to the body." – Brian Tracy`
 ];
 
-let mentalArray = [
+let mental = [
     'Do your best to enjoy 15 minutes of sunshine.',
     'Take 30 minutes to go for a walk in nature.',
     'Do something with friends and family.',
@@ -24,7 +26,7 @@ let mentalArray = [
     'Feeling anxious?  Take a trip down memory lane and do some coloring.'
 ];
 
-let wellnessArray = [
+let wellness = [
     'Get enough sleep',
     'Take care of your gut health with probiotics and fiber',
     'Drink some water, especially before meals',
@@ -35,3 +37,17 @@ let wellnessArray = [
     'Eat eggs, yolk and all',
     'Don’t drink sugar calories'
 ];
+
+const randomizeMessage = () => {
+    let mixedArray = [];
+    mixedArray = mixedArray.concat(quote, mental, wellness);
+    let message = mixedArray[Math.floor(Math.random() * mixedArray.length)];
+    return message;
+};
+// create  a function to display the message
+function displayMessage(){
+    window.document.getElementById("message").innerHTML = randomizeMessage();
+}
+
+// add event and call the display message function
+window.addEventListener('click', displayMessage);
